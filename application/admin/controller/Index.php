93e8ecc13c49c2f87636993542e5f51221e789d2
sunlupeng2020 <slp2060@163.com>
2020-06-05 17:23:57 +0800

    添加了对用户名、密码和验证码的验证

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -3,10 +3,24 @@ namespace app\admin\controller;
 
 use think\Controller;
 
+use app\admin\validate\AdminUser as UserValidate;
+
 class Index extends Controller
 {
     public function login()
     {
+        if($this->request->isPost()){
+            $data = [
+                'username' => $this->request->post('username/s','','trim'),
+                'password' => $this->request->post('password/s',''),
+                'captcha' => $this->request->post('captcha/s', '','trim')
+            ];
+            $validate = new UserValidate;
+            if (!$validate->scene('login')->check($data)) {
+                $this->error('登录失败：' . $validate->getError() . '。');
+            }
+            $this->success('登录成功.');
+        }
         return $this->fetch();
     }
 
---
+++ application/admin/validate/AdminUser.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\admin\validate;
+
+use think\Validate;
+
+class AdminUser extends Validate
+{
+    protected $rule = [
+        'username' => 'require|max:32',
+        'password' => 'require|min:6'
+    ];
+    protected $message = [
+        'username.require' => '用户名不能为空',
+        'username.max' => '用户名最多为32个字符',
+        'password.require' => '密码不能为空',
+        'password.min' => '密码最少为6位',
+        'captcha.require' => '验证码不能为空',
+        'captcha.captcha' => '验证码有误'
+
+    ];
+    public function sceneLogin()
+    {
+        return $this->append('captcha','require|captcha');
+    }
+}
