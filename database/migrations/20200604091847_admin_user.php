8519e7fcb8dd258c9ec3c686cb8b52c0471507fb
sunlupeng2020 <slp2060@163.com>
2020-06-06 19:41:29 +0800

    创建admin表的数据库迁移文件修改、保存，

--- database/migrations/20200604091847_admin_user.php
+++ database/migrations/20200604091847_admin_user.php
@@ -31,7 +31,6 @@ class AdminUser extends Migrator
         $table = $this->table('admin_user',['engine'=>'InnoDB', 'collation' => 'utf8mb4_general_ci']);
         $table ->addColumn('admin_role_id', 'integer', ['null' => false, 'default' =>0, 'comment' =>'角色id'])
         ->addColumn('username', 'string',['limit' => 32, 'null' => false, 'default' => '','comment' => '用户名'])
-        //->addColumn('password', 'string',['limit' => 32, 'null' => false, 'default' => '','comment' => '密码'])
         ->addColumn('salt', 'char', ['limit' => 32, 'null' =>false, 'default'=>'','comment' =>'密码salt'])
         ->addIndex(['username'],['unique' =>true])
         ->addTimestamps()
